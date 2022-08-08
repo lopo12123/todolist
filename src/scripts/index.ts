d73dd1c11fe30cfd5997bb539ae614f9de049aab
lopo <lopo@zju.edu.cn>
2022-08-08 18:14:12 +0800

    feat: indexedDB

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,12 +1,30 @@
 class UseDB {
-    // 数据库名称
+    /**
+     * @description 数据库实例
+     * @private
+     */
+    #db: IDBDatabase | null = null
+
+    /**
+     * @description 数据库名称
+     * @private
+     */
     readonly #dbName: string
     get dbName() {
         return this.#dbName
     }
 
-    // 数据库实例
-    #db: IDBDatabase | null = null
+    /**
+     * @description 获取全部的数据表名称
+     */
+    get storeNames() {
+        const nameList = this.#db?.objectStoreNames
+        if(!nameList) return []
+
+        const nameArray: string[] = []
+        for (let i = 0; i < nameList.length; i++) nameArray.push(nameList[i])
+        return nameArray
+    }
 
     constructor(dbName: string) {
         if(!window.indexedDB) throw new Error('Not Support')
@@ -14,19 +32,44 @@ class UseDB {
     }
 
     /**
+     * @description 获取数据库信息列表 (名称、版本)
+     */
+    static dbInfos() {
+        return window.indexedDB.databases()
+    }
+
+    /**
+     * @description 删除数据库
+     * @param dbName 数据库名称
+     */
+    static dropDb(dbName: string) {
+        return new Promise<Event>((resolve, reject) => {
+            const dbOperate = window.indexedDB.deleteDatabase(dbName)
+            dbOperate.onsuccess = (ev) => {
+                resolve(ev)
+            }
+            dbOperate.onerror = () => {
+                reject('删除数据库出错')
+            }
+        })
+    }
+
+    /**
      * @description 打开数据库, 创建表
      * @param stores 数据表列表(名称, 主键)
      */
     init(stores: { storeName: string, pk?: string }[]) {
-        return new Promise((resolve, reject) => {
-            if(stores.length === 0) reject(new Error('至少需要创建一张表!'))
+        return new Promise<UseDB>((resolve, reject) => {
+            if(stores.length === 0) reject('至少需要创建一张表!')
             else {
                 const conn = indexedDB.open(this.#dbName)
                 conn.onsuccess = () => {
                     this.#db = conn.result
                     resolve(this)
                 }
-                conn.onerror = reject
+                conn.onerror = () => {
+                    reject('打开数据库出错')
+                }
                 conn.onupgradeneeded = () => {
                     const db = conn.result
                     stores.forEach(({ storeName, pk }) => {
@@ -43,18 +86,20 @@ class UseDB {
      * @param data 新增数据项
      */
     insert(storeName: string, data: any) {
-        return new Promise((resolve, reject) => {
+        return new Promise<UseDB>((resolve, reject) => {
             const db = this.#db
-            if(!db) reject(new Error('无数据库对象'))
+            if(!db) reject('无数据库对象')
             else {
-                const idbOperate = db
+                const dbOperate = db
                     .transaction(storeName, 'readwrite')
                     .objectStore(storeName)
                     .add(data)
-                idbOperate.onsuccess = () => {
+                dbOperate.onsuccess = () => {
                     resolve(this)
                 }
-                idbOperate.onerror = reject
+                dbOperate.onerror = () => {
+                    reject('插入数据出错')
+                }
             }
         })
     }
@@ -65,18 +110,20 @@ class UseDB {
      * @param data 更新数据项
      */
     update(storeName: string, data: any) {
-        return new Promise((resolve, reject) => {
+        return new Promise<UseDB>((resolve, reject) => {
             const db = this.#db
-            if(!db) reject(new Error('无数据库对象'))
+            if(!db) reject('无数据库对象')
             else {
-                const idbOperate = db
+                const dbOperate = db
                     .transaction(storeName, 'readwrite')
                     .objectStore(storeName)
                     .put(data)
-                idbOperate.onsuccess = () => {
+                dbOperate.onsuccess = () => {
                     resolve(this)
                 }
-                idbOperate.onerror = reject
+                dbOperate.onerror = () => {
+                    reject('更新数据出错')
+                }
             }
         })
     }
@@ -86,19 +133,21 @@ class UseDB {
      * @param storeName 表名
      * @param pk 删除项的主键
      */
-    delete(storeName: string, pk: string) {
-        return new Promise((resolve, reject) => {
+    remove(storeName: string, pk: string) {
+        return new Promise<UseDB>((resolve, reject) => {
             const db = this.#db
-            if(!db) reject(new Error('无数据库对象'))
+            if(!db) reject('无数据库对象')
             else {
-                const idbOperate = db
+                const dbOperate = db
                     .transaction(storeName, 'readwrite')
                     .objectStore(storeName)
                     .delete(pk)
-                idbOperate.onsuccess = () => {
+                dbOperate.onsuccess = () => {
                     resolve(this)
                 }
-                idbOperate.onerror = reject
+                dbOperate.onerror = () => {
+                    reject('删除数据出错')
+                }
             }
         })
     }
@@ -109,25 +158,125 @@ class UseDB {
      * @param pk 查询项的主键
      */
     query(storeName: string, pk: string) {
-        return new Promise((resolve, reject) => {
+        return new Promise<UseDB>((resolve, reject) => {
             const db = this.#db
-            if(!db) reject(new Error('无数据库对象'))
+            if(!db) reject('无数据库对象')
             else {
-                const idbOperate = db
+                const dbOperate = db
                     .transaction(storeName, 'readonly')
                     .objectStore(storeName)
                     .get(pk)
-                idbOperate.onsuccess = () => {
+                dbOperate.onsuccess = () => {
                     resolve(this)
                 }
-                idbOperate.onerror = reject
+                dbOperate.onerror = () => {
+                    reject('查询数据出错')
+                }
             }
         })
     }
 
-    // 关闭
+    /**
+     * @description 获取数据表中的全部数据
+     * @param storeName 表名
+     */
+    getStoreAll(storeName: string) {
+        return new Promise<any[]>((resolve, reject) => {
+            const db = this.#db
+            if(!db) reject('无数据库对象')
+            else {
+                const dbOperate = db
+                    .transaction(storeName, 'readonly')
+                    .objectStore(storeName)
+                    .getAll()
+                dbOperate.onsuccess = () => {
+                    resolve(dbOperate.result)
+                }
+                dbOperate.onerror = () => {
+                    reject('删除数据出错')
+                }
+            }
+        })
+    }
+
+    /**
+     * @description 获取数据库中的全部数据
+     */
+    getStoresAll() {
+        const db = this.#db
+        if(!db) return Promise.reject('无数据库对象')
+        else {
+            const storeNames = this.storeNames
+            const operates: Promise<any[]>[] = storeNames.map(storeName => {
+                return new Promise<any[]>(resolve => {
+                    const dbOperate = db
+                        .transaction(storeName, 'readonly')
+                        .objectStore(storeName)
+                        .getAll()
+                    dbOperate.onsuccess = () => {
+                        resolve(dbOperate.result)
+                    }
+                    dbOperate.onerror = () => {
+                        resolve([])
+                    }
+                })
+            })
+
+            return new Promise<{ [storeName: string]: any[] }>((resolve, reject) => {
+                Promise.all(operates)
+                    .then(list => {
+                        const dbData: { [k: string]: any[] } = {}
+                        storeNames.forEach((storeName, idx) => {
+                            dbData[storeName] = list[idx]
+                        })
+                        resolve(dbData)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        }
+    }
+
+    /**
+     * @description 删除数据表
+     * @param storeName
+     */
+    dropStore(storeName: string) {
+        const db = this.#db
+        if(!db) return Promise.reject('无数据库对象')
+        else {
+            return new Promise<UseDB>((resolve, reject) => {
+                const oldVersion = db.version
+                const dbOperate = window.indexedDB.open(this.#dbName, oldVersion + 1)
+                dbOperate.onsuccess = () => {
+                    const newDB = dbOperate.result
+                    this.#db = newDB
+                    newDB.close()
+                    resolve(this)
+                }
+                dbOperate.onupgradeneeded = (e) => {
+                    dbOperate.result.deleteObjectStore(storeName)
+                }
+                dbOperate.onerror = () => {
+                    reject('删除数据表出错')
+                }
+            })
+        }
+    }
+
+    /**
+     * @description 关闭数据库
+     */
     close() {
         this.#db?.close()
+    }
+
+    /**
+     * @description 删除数据库
+     */
+    dispose() {
+        return UseDB.dropDb(this.#dbName)
     }
 }
 
