758e7e60e1bba9d89240a08192d992143fd17d54
lopo <lopo@zju.edu.cn>
2022-08-09 11:15:57 +0800

    chore: vue3-seamless-scroll

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import "./styles/index.css";
 import { createApp } from "vue";
 import { router } from "@/routers";
+import Vue3SeamlessScroll from "vue3-seamless-scroll";
 import App from "./App.vue";
 
 createApp(App)
     .use(router)
+    .use(Vue3SeamlessScroll)
     .mount('#app')
