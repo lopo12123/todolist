0855f5ac31b0e69866a87aa14cd927fe2200a307
lopo <lopo@zju.edu.cn>
2022-08-09 18:03:38 +0800

    feat: type of record

--- src/scripts/useTodo.ts
+++ src/scripts/useTodo.ts
@@ -3,7 +3,42 @@ import { UseDB } from "./useDB";
 const enum DBStatic {
     dbName = 'todoDB',
     storeName = 'todoStore',
-    pk = 't'
+    pk = 'due'
+}
+
+/**
+ * @description 数据库记录结构
+ */
+type TodoRecord = {
+    /**
+     * @description deadline of the item (in timestamp) - pk
+     */
+    due: number
+    /**
+     * @description time of when the item was set (in timestamp)
+     */
+    birth: number
+    /**
+     * @description if finished
+     */
+    finish: boolean
+    /**
+     * @description brief description
+     */
+    desc: string
+    /**
+     * @description document of this to-do-item
+     */
+    docId: string | null
+}
+/**
+ * @description 统计总览结构
+ */
+type OverviewResult = {
+    total: number
+    done: number
+    undo: number
+    late: number
 }
 
 class TODOList {
@@ -13,6 +48,10 @@ class TODOList {
         this.#dbc = new UseDB(DBStatic.dbName)
     }
 
+    // region 初始化
+    /**
+     * @description 连接(不存在则创建)数据库
+     */
     setup() {
         return this.#dbc
             .open([
@@ -20,9 +59,18 @@ class TODOList {
             ])
     }
 
-    overview() {
+    // endregion
 
+    // region 统计总览
+    /**
+     * @description 统计总览
+     * @param range 未来一周 / 未来一月 / 过去一周 / 过去一月 / 全部
+     */
+    overview(range: 'next-week' | 'next-month' | 'last-week' | 'last-month' | 'all') {
+        const todayStart = new Date(new Date().toDateString()).getTime()
     }
+
+    // endregion
 }
 
 // 使用单例
--- src/views/Home/OverviewList.vue
+++ src/views/Home/OverviewList.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 
-const useDB =
 </script>
 
 <template>
