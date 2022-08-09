30ad7dbdc0032da515eb98af8044d36349240ac0
lopo <lopo@zju.edu.cn>
2022-08-09 17:24:14 +0800

    feat: 窗口大小 关闭窗口

--- src/scripts/useTauri.ts
+++ src/scripts/useTauri.ts
@@ -1,5 +1,7 @@
 import { appWindow, LogicalSize } from "@tauri-apps/api/window";
+import type { Router } from "vue-router";
 
+// region 设置窗口大小
 const WindowSizeMap = {
     Home: [ 400, 200 ],
     Calendar: [ 600, 400 ],
@@ -9,7 +11,22 @@ const resizeWindow = (to: 'Home' | 'Calendar' | 'OrderList') => {
     const [ w, h ] = WindowSizeMap[to]
     return appWindow.setSize(new LogicalSize(w, h))
 }
+// endregion
+
+// region 关闭窗口
+const closeWindow = (router: Router) => {
+    return new Promise((resolve, reject) => {
+        router.push({ name: 'Blank' })
+        setTimeout(() => {
+            appWindow.close()
+                .then(resolve)
+                .catch(reject)
+        }, 500)
+    })
+}
+// endregion
 
 export {
-    resizeWindow
+    resizeWindow,
+    closeWindow,
 }
