5c34698d761270365c659447e9c64da4a1a67467
lopo <lopo@zju.edu.cn>
2022-08-11 22:21:34 +0800

    feat: 布局框架

---
+++ src/layouts/WithBanner.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="with-banner-layout">
+        <div class="banner-container">
+            <slot name="banner"/>
+        </div>
+        <div class="body-container">
+            <slot name="body"/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.with-banner-layout {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    .banner-container {
+        position: relative;
+        width: 100%;
+        height: 40px;
+        border-bottom: solid 1px #ccc;
+    }
+
+    .body-container {
+        position: relative;
+        width: 100%;
+        height: calc(100% - 40px);
+    }
+}
+</style>
