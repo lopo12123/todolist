d0603116829175ae76ebbc303ec8f646bec75c3d
lopo <lopo@zju.edu.cn>
2022-08-11 22:37:24 +0800

    style: 统一动画时间

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <router-view v-slot="{Component}">
-        <transition name="collapse" mode="out-in">
+        <transition name="collapse" mode="out-in" duration="500">
             <component :is="Component"/>
         </transition>
     </router-view>
